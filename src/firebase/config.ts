1001178887dac1273f4a17389de7eee177a4f65e
Wanjing Tan <RiverDiamond11@gmail.com>
2026-02-05 10:50:46 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-2601315998-ecccb",
+  "appId": "1:746122566992:web:412b54f293d0f836c3e055",
+  "apiKey": "AIzaSyAKJC7zX0nEwk3nGxsdn_X1MCQwqkcJeVY",
+  "authDomain": "studio-2601315998-ecccb.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "746122566992"
+};
