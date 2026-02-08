273336edfe0a59f692b13e4dde6246c3f47e8401
Wanjing Tan <RiverDiamond11@gmail.com>
2026-02-08 13:14:22 +0000

    why the sentence Emakume bat da. did not produce media?

--- src/ai/flows/text-to-speech-flow.ts
+++ src/ai/flows/text-to-speech-flow.ts
@@ -69,7 +69,9 @@ const synthesizeSpeechFlow = ai.defineFlow(
         config: {
             responseModalities: ['AUDIO'],
             speechConfig: {
+              voiceConfig: {
                 languageCode: 'eu-ES',
+              }
             }
         },
         prompt: text,
